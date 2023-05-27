17cc29650710c171146b82678341af008d2bbdbe
Junhao Jerry Zhang <jerry_mountain@hotmail.com>
2023-05-27 12:57:18 -0500

    get some testts going

---
+++ modes/tests.js
@@ -0,0 +1,17 @@
+const tests = {
+    "Left Hand DV": {
+        "list": ["a","o","e","u","'",",",".","p","y","i",";","q","j","k","x","1","2","3","4","5","6"],
+        "noSpace": true,
+    },
+    /*
+    "Right Hand DV": {
+
+    },
+    "Left Hand QWERTY": {
+
+    },
+    "Right Hand QWERTY": {
+
+    },
+    */
+};
