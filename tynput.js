6814f80a67ccaaddaa8853cbd08cca0d5740cd73
Junhao Jerry Zhang <jerry_mountain@hotmail.com>
2023-05-28 09:35:47 -0500

    finish - ish tynput

--- tynput.js
+++ tynput.js
@@ -1,3 +1,5 @@
+import { TestResult } from "./test-result";
+
 /*
 Typing Input Manager
 This manages the text box and what the user sees.
@@ -31,14 +33,61 @@ class TynputManager {
         this.inputEl.addEventListener("blur", () => {
             this.focusOverlay.style.display = "flex";
         });
+
+        this.inputEl.addEventListener("keydown", e => {
+            const idx = this.cIdx;
+            if (e.key == "Backspace") {
+                if (idx == 0) return;
+                this.els[idx].classList.remove("curr", "wrong", "correct");
+                idx--;
+                this.els[idx].classList.remove("wrong", "correct");
+                this.els[idx].classList.add("curr");
+            }
+
+            // not a typed key, or command (could happen!)
+            if (e.key.length != 1 || e.altKey || e.ctrlKey || e.metaKey) {
+                return;
+            }
+
+            if (idx == 0) {
+                this.testResult.begin();
+            }
+
+            this.els[idx].classList.remove("curr");
+
+            if (this.text[idx] == e.key) {
+                this.els[idx].classList.add("correct");
+                this.testResult.correct++;
+            } else {
+                this.els[idx].classList.add("wrong");
+                this.testResult.wrong++;
+            }
+
+            this.testResult.chars++;
+            idx++;
+            if (idx >= this.els.length) {
+                this.testResult.end();
+                this.listener.onEnd(this.testResult, this);
+            }
+
+            this.els[idx].classList.add("curr");
+        });
     }
 
-    newTest(text) {
-        this.testIdx = 0;
-        this.testText = text;
+    /**
+     * Create a new test
+     * @param {string} title title
+     * @param {string} text text
+     * @param {TynputListener} listener listener
+     */
+    newTest(title, text, listener = this.listener) {
+        this.cIdx = 0;
+        this.text = text;
+        this.listener = listener;
+        this.testResult = new TestResult(title);
 
         const chars = text.split("");
-        const els = [];
+        this.els = [];
         for (const char of chars) {
             const el = document.createElement("pre");
             el.textContent = char;
@@ -48,18 +97,23 @@ class TynputManager {
                 el.classList.add("space");
             }
 
-            els.push(el);
+            this.els.push(el);
         }
 
-        els[0].classList.add("curr");
-        typingCont.append(...els);
+        this.els[0].classList.add("curr");
+        this.testCont.append(...this.els);
     }
 }
 
-export const tynput = new TynputManager();
+const tynput = new TynputManager();
 
 export class TynputListener {
     constructor() {
         this.tynput = tynput;
+    }
+
+    onEnd(_testResult, _manager) { }
+    clear() {
+        // todos
     }
 }
