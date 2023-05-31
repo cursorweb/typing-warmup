8454f1395d139773fc671d3996453cf7e1b97a74
Junhao Jerry Zhang <jerry_mountain@hotmail.com>
2023-05-30 19:37:48 -0500

    last update beforeTS!!

--- webpack.config.js
+++ webpack.config.js
@@ -2,6 +2,7 @@ const path = require('path');
 
 module.exports = {
     entry: './src/main.js',
+    devtool: "eval-source-map",
     output: {
         filename: 'main.js',
         path: path.resolve(__dirname, 'dist'),
